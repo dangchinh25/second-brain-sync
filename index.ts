71092a5be8a27fc7c59beac9244934ef5e7a1bee
dangchinh25 <dangchinh2501@gmail.com>
2023-07-03 10:27:36 -0500

    Add getAllFiles function

--- index.ts
+++ index.ts
@@ -1,1 +1,27 @@
-console.log('This is a template repo')
+import fs from 'fs'
+import path from 'path'
+
+function getAllFiles(folderPath: string): string[] {
+    const files: string[] = [];
+    function traverseDirectory(directory: string) {
+        const entries = fs.readdirSync(directory, { withFileTypes: true });
+
+        for (const entry of entries) {
+            const entryPath = path.join(directory, entry.name);
+    
+            if (entry.isFile()) {
+            files.push(entryPath);
+            } else if (entry.isDirectory()) {
+            traverseDirectory(entryPath);
+            }
+        }
+    }
+
+    traverseDirectory(folderPath);
+    return files;
+}
+
+const folderPath = '/Users/chinhle/Documents/Learning/SecondBrain'
+const filesInFolder = getAllFiles(folderPath)
+
+console.log(filesInFolder)
