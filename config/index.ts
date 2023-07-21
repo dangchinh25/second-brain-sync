6630bc08b329cdcf258c23905db99fbfba792a54
dangchinh25 <dangchinh2501@gmail.com>
2023-07-21 16:57:06 -0400

    Init google auth library and access drive

---
+++ config/index.ts
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import dotenv from 'dotenv';
+import {
+    cleanEnv
+    , str
+    , num
+} from 'envalid';
+
+dotenv.config( { path: '.env' } );
+
+export const env = cleanEnv( process.env, {
+    PORT: num( { default: 3000 } )
+    , CLIENT_ID: str( { default: undefined } )
+    , CLIENT_SECRET: str( { default: undefined } )
+    , REDIRECT_URI: str( { default: 'http://localhost:3000/oauth2callback' } )
+    , TOKEN_CODE_PATH: str( { default: 'token-code.json' } )
+    , SCOPE: str()
+} );
--- index.ts
+++ index.ts
@@ -10,6 +10,12 @@ import {
 import { getFileAsString } from './utils';
 import { FileChanges } from './lib/octokit';
 
+import { OAuth2Client, OAuth2ClientOptions } from 'google-auth-library';
+import open from 'open';
+import { app } from './server';
+import { google } from 'googleapis';
+import { env } from './config';
+
 interface FileNameWithPath {
     fileName: string;
     filePath: string;
@@ -172,4 +178,48 @@ const test = async () => {
     console.log( mergeRequestResponse.value );
 };
 
-test();
+const main = async () => {
+    const server = app.listen( env.PORT, () => {
+        console.log( `Server running on port ${ env.PORT }` );
+    } );
+
+    const oAuth2ClientOptions: OAuth2ClientOptions = {
+        clientId: env.CLIENT_ID
+        , clientSecret: env.CLIENT_SECRET
+        , redirectUri: env.REDIRECT_URI
+    };
+
+    const oAuth2Client = new OAuth2Client( oAuth2ClientOptions );
+
+    // Generate the url that will be used for the consent dialog.
+    const authorizeUrl = oAuth2Client.generateAuthUrl( {
+        [ 'access_type' ]: 'offline'
+        , scope: env.SCOPE
+    } );
+
+    open( authorizeUrl, { wait: false } ).then( cp => cp.unref() );
+
+    while ( !fs.existsSync( env.TOKEN_CODE_PATH ) ) {
+        console.log( 'TokenCode has not been set. Try again in 2 second.' );
+
+        await new Promise( resolve => setTimeout( resolve, 2000 ) );
+    }
+
+    const tokenCodePayload = JSON.parse( fs.readFileSync( env.TOKEN_CODE_PATH ).toString() );
+
+    const getTokenResponse = await oAuth2Client.getToken( tokenCodePayload.tokenCode );
+
+    oAuth2Client.setCredentials( getTokenResponse.tokens );
+
+    const drive = google.drive( { version: 'v3', auth: oAuth2Client } );
+    const res = await drive.files.list();
+
+    const files = res.data.files;
+
+    console.log( 'Files:' );
+    files?.map( ( file ) => {
+        console.log( `${ file.name } (${ file.id })` );
+    } );
+};
+
+main();
