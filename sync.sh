ab54999a15b433db1c4cfc2fead7c96a96d338d5
dangchinh25 <dangchinh2501@gmail.com>
2024-09-02 00:48:56 -0700

    raycast script command

---
+++ sync.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Sync Obsidian
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.author dang_chinh
+# @raycast.authorURL https://raycast.com/dang_chinh
+
+# This script is used to sync the local Obsidian vault with Github https://github.com/dangchinh25/second-brain
+
+# Run the npm run dev command
+source ~/.zshrc
+npm run dev
+
